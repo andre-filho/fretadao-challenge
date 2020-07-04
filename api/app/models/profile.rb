2dbde2b9a14bdd6855750b6ff991a6b9fbb277af
Andre Filho <andre.filho001@outlook.com>
2020-07-04 17:45:40 -0300

    FIX: get_additional_profile_info method to scrap correct information

--- api/app/models/profile.rb
+++ api/app/models/profile.rb
@@ -13,21 +13,24 @@ class Profile < ApplicationRecord
 
   validates :image_url, presence: true
 
-  before_validation do
-    # ensure all fields are present during validation
-    shrink_url
-    get_additional_profile_info
-  end
+  before_validation :get_additional_profile_info
+  before_save :shrink_url
 
   private
 
   def get_additional_profile_info
     # call scrapper
-    raw_page = HTTParty.post(url)
+    raw_page = HTTParty.get(url)
     parsed_page = Nokogiri::HTML(raw_page.body)
-    self.gh_username = parsed_page.css('h1.v-card-names span.p-nickname.vcard-username')
+    self.gh_username = parsed_page.css('.p-nickname').text
+    self.gh_followers = parsed_page.css('span.text-bold.text-gray-dark').first.text.to_i
+    self.gh_subscriptions = parsed_page.css('span.text-bold.text-gray-dark')[1].text.to_i
+    self.gh_stars = parsed_page.css('span.text-bold.text-gray-dark')[2].text.to_i
+    self.gh_contributions = parsed_page.css('h2.f4.text-normal.mb-2')[1].text[/[0-9]+/].to_i
+    self.image_url = parsed_page.css('img.avatar.avatar-user.border').first['src']
   end
 
+  # TODO: shrink img url too
   def shrink_url
     if url.match(/(github\.com\/\w+)/)
       # u is the input's name on shorturl
@@ -35,10 +38,6 @@ class Profile < ApplicationRecord
       parsed_page = Nokogiri::HTML(raw_page.body)
 
       # the shortned url is located inside an input
-      print '-----------------------------------------------------------------\n'
-      print parsed_page
-      print '-----------------------------------------------------------------\n'
-
       self.url = parsed_page.css('#shortenurl').first['value'] || ''
 
     elsif url.match(/(https:\/\/shorturl\.at\/shortener\.php)/)
