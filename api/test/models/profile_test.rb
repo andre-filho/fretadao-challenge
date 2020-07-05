bf0ade96f71caf71acd8fb134871c1c4918f4395
Andre Filho <andre.filho001@outlook.com>
2020-07-05 18:38:58 -0300

    FIX: cors and begin describing test cases

--- api/test/models/profile_test.rb
+++ api/test/models/profile_test.rb
@@ -1,11 +1,40 @@
 require 'test_helper'
 
 class ProfileTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 
-  test "create user" do
+  test "should create profile" do
+    Profile.create()
   end
 
+  test "should not create profile without name" do
+
+  end
+
+  test "should not create profile without github username" do
+
+  end
+
+  test "should not create profile without url" do
+
+  end
+
+  test "should not create profile without image url" do
+
+  end
+
+  test "should not create profile without github followers" do
+
+  end
+
+  test "should not create profile without github stars" do
+
+  end
+
+  test "should not create profile without github contributions" do
+
+  end
+
+  test "should raise validation error in case of unexisting github profile" do
+
+  end
 end
