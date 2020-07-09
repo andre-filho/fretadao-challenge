f48406667d11236bd9d832b101e4b69d1bb8ad29
Andre Filho <andre.filho001@outlook.com>
2020-07-09 09:35:21 -0300

    ADD: create profile fab button

--- client/src/App.vue
+++ client/src/App.vue
@@ -30,17 +30,28 @@
 
     <v-main>
       <router-view/>
+      <add-profile-fab @createdEvent="handleProfileCreation"/>
     </v-main>
+
   </v-app>
 </template>
 
 <script>
+import AddProfileFab from '@/components/AddProfileFab'
 
 export default {
   name: 'App',
 
-  data: () => ({
-    //
-  })
+  components: {
+    AddProfileFab
+  },
+
+  methods: {
+    handleProfileCreation (value) {
+      this.$router.push({
+        name: 'profileView', params: { id: value.id, profile: value }
+      })
+    }
+  }
 }
 </script>
--- client/src/api/profiles-api.js
+++ client/src/api/profiles-api.js
@@ -3,6 +3,10 @@ import API from './api'
 const endpointRoot = '/profiles'
 
 const ProfilesAPI = {
+  createProfile (profile) {
+    return API.post(`${endpointRoot}/`, profile)
+  },
+
   getProfile (id) {
     return API.get(`${endpointRoot}/${id}/`)
   },
